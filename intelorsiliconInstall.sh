25fe8c98f2921c879230544160e7076666f692f7
andrew-kuntz1 <134640416+andrew-kuntz1@users.noreply.github.com>
2024-10-25 12:00:30 -0500

    Create intelorsiliconInstall.sh

---
+++ intelorsiliconInstall.sh
@@ -0,0 +1,29 @@
+# Installs zoom as needed on Apple Silicon or Intel macs.
+
+exitcode=0
+
+# Determine OS version
+# Save current IFS state
+
+OLDIFS=$IFS
+
+IFS='.' read osvers_major osvers_minor osvers_dot_version <<< "$(/usr/bin/sw_vers -productVersion)"
+
+# restore IFS to previous state
+
+IFS=$OLDIFS
+
+  # Check the processor
+
+  processor=$(/usr/sbin/sysctl -n machdep.cpu.brand_string | grep -o "Intel")
+  
+  if [[ -n "$processor" ]]; then
+	echo "Installing Intel Package"
+	installer -pkg /private/tmp/zoominstaller/Intel/ZoomIntel.pkg -target /
+	echo "done installing Intel package"
+  else
+	echo "Installing M1 package"
+	installer -pkg /private/tmp/zoominstaller/M1/ZoomM1.pkg -target /
+	echo "done installing m1 package"
+  fi   
+exit $exitcode
